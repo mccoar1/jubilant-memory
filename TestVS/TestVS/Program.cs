a3c75ed8079e477960e72808de126975dcd9ddc0
Alfie McCormack <mccoar1@student.op.ac.nz>
2026-05-07 18:57:09 +1200

    Had a spacing error in code, fixed it

--- TestVS/TestVS/Program.cs
+++ TestVS/TestVS/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-	    Console.Writeline("I did this on Git Bash");
+            Console.Writeline("I did this on Git Bash");
             Console.WriteLine("Using nano Program.cs");
             Console.ReadLine();	
         }
