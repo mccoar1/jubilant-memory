22ed5490fe2c1e0cc1c778c5de9dbb294e7710fd
Alfie McCormack <mccoar1@student.op.ac.nz>
2026-05-07 18:55:06 +1200

    Made a VS, edited it in nano Program.cs

--- TestVS/TestVS/Program.cs
+++ TestVS/TestVS/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+	    Console.Writeline("I did this on Git Bash");
+            Console.WriteLine("Using nano Program.cs");
+            Console.ReadLine();	
         }
     }
 }
